c7146007d7b234655333a7418405e9595a5dd805
陆放明 <1071956678@qq.com>
2018-07-14 09:09:00 +0800

    7.14

---
+++ SolutionQ_085 二叉搜索树节点插入.java
@@ -0,0 +1,45 @@
+package Questions;
+
+import sun.reflect.generics.tree.Tree;
+
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ * public int val;
+ * public TreeNode left, right;
+ * public TreeNode(int val) {
+ * this.val = val;
+ * this.left = this.right = null;
+ * }
+ * }
+ */
+class TreeNode {
+    public int val;
+    public TreeNode left, right;
+
+    public TreeNode(int val) {
+        this.val = val;
+        this.left = this.right = null;
+    }
+}
+
+public class SolutionQ_085 {
+    /**
+     * @param root: The root of the binary search tree.
+     * @param node: insert this node into the binary search tree
+     * @return: The root of the new binary search tree.
+     */
+    public TreeNode insertNode(TreeNode root, TreeNode node) {
+        if (root == null) {
+            return node;
+        }
+        int targetVal = node.val;
+        if (targetVal < root.val)
+            root.left = insertNode(root.left, node);
+        else if (targetVal > root.val)
+            root.right = insertNode(root.right, node);
+        return root;
+    }
+
+
+}
