4bdeef1a78bd3420ea6a9334d803af3b79dffd38
lfm <1071956678@qq.com>
2019-02-20 18:24:01 +0800

    2.20

---
+++ src/leet/Solution2019_2/Trie.py
@@ -0,0 +1,37 @@
+class Trie:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+
+    def insert(self, word: 'None') -> 'None':
+        """
+        Inserts a word into the trie.
+        """
+        if word == None:
+            return
+
+    def search(self, word: 'str') -> 'bool':
+        """
+        Returns if the word is in the trie.
+        """
+
+    def startsWith(self, prefix: 'str') -> 'bool':
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        """
+
+
+class Test:
+    def run(self, value: 'int' = 3) -> int:
+        print(value)
+        if value:
+            pass
+        else:
+            return value
+
+
+t = Test()
+v = t.run()
+print(v)
