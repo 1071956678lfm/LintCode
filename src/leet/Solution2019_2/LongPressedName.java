443c97b54fee8129b7620f5ed07f2f9ce65edf6b
lfm <1071956678@qq.com>
2019-02-28 20:49:45 +0800

    2.28

---
+++ src/leet/Solution2019_2/LongPressedName.java
@@ -0,0 +1,16 @@
+package leet.Solution2019_2;
+
+//925
+public class LongPressedName {
+    /**
+     * Your friend is typing his name into a keyboard.  Sometimes, when typing a character c,
+     * the key might get long pressed, and the character will be typed 1 or more times.
+     * <p>
+     * You examine the typed characters of the keyboard.
+     * Return True if it is possible that it was your friends name,
+     * with some characters (possibly none) being long pressed.
+     */
+    public boolean isLongPressedName(String name, String typed) {
+
+    }
+}
