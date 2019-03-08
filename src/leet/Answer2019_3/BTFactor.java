487e7b5850d969019febd877566667a7b6640f75
lfm <1071956678@qq.com>
2019-03-08 23:29:34 +0800

    not known

---
+++ src/leet/Answer2019_3/BTFactor.java
@@ -0,0 +1,28 @@
+package leet.Answer2019_3;
+
+//823
+public class BTFactor {
+    /**
+     * Given an array of unique integers, each integer is strictly greater than 1.
+     * <p>
+     * We make a binary tree using these integers and each number may be used for any number of times.
+     * <p>
+     * Each non-leaf node's value should be equal to the product of the values of it's children.
+     * <p>
+     * How many binary trees can we make?  Return the answer modulo 10 ** 9 + 7.
+     * <p>
+     * Example 1:
+     * <p>
+     * Input: A = [2, 4]
+     * Output: 3
+     * Explanation: We can make these trees: [2], [4], [4, 2, 2]
+     * Example 2:
+     * <p>
+     * Input: A = [2, 4, 5, 10]
+     * Output: 7
+     * Explanation: We can make these trees: [2], [4], [5], [10], [4, 2, 2], [10, 2, 5], [10, 5, 2].
+     */
+    public int numFactoredBinaryTrees(int[] A) {
+
+    }
+}
