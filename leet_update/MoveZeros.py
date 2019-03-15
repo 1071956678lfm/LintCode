4213fbdfa8c26e8fb6253bcf56b7993a0961bd30
lfm <1071956678@qq.com>
2019-03-15 22:59:37 +0800

    m

---
+++ leet_update/MoveZeros.py
@@ -0,0 +1,13 @@
+# 283
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        length = len(nums)
+        
+
+arr = [0, 1, 0, 3, 12]
+Solution().moveZeroes(arr)
+print(arr)
