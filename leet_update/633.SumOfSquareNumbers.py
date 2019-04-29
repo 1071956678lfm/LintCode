8e2c19a1d316d9066835fbf8ec32082eac2d415a
lfm <1071956678@qq.com>
2019-04-29 22:35:59 +0800

    Q633

--- leet_update/633.SumOfSquareNumbers.py
+++ leet_update/633.SumOfSquareNumbers.py
@@ -1,3 +1,18 @@
 class Solution:
     def judgeSquareSum(self, c: int) -> bool:
-        pass
+        import math
+        if c == 0:
+            return True
+        i = 0
+        j = int(math.sqrt(c))
+        while i <= j:
+            if i ** 2 + j ** 2 == c:
+                return True
+            elif i ** 2 + j ** 2 > c:
+                j -= 1
+            else:
+                i += 1
+        return False
+
+
+print(Solution().judgeSquareSum(1000))
