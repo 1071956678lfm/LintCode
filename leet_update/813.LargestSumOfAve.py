50e89699389a9121b44cdc40b35072dca109ab39
lfm <1071956678@qq.com>
2019-05-03 21:36:36 +0800

    930 BinSumSubarr

---
+++ leet_update/813.LargestSumOfAve.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+class Solution:
+    def largestSumOfAverages(self, A: List[int], K: int) -> float:
+        pass
+
+
+print('as')
--- leet_update/930.BinarySumarraysWithSum.py
+++ leet_update/930.BinarySumarraysWithSum.py
@@ -1,30 +1,18 @@
+import collections
 from typing import List
 
 
 class Solution:
     def numSubarraysWithSum(self, A: List[int], S: int) -> int:
-        ans, i, j, cur_sum = 0, 0, 0, 0
-        cur_sum = A[0]
-        while j < len(A):
-            if cur_sum == S:
-                ans += 1
-                j += 1
-                cur_sum += A[j] if j < len(A) else 0
-            elif cur_sum < S:
-                j += 1
-                cur_sum += A[j] if j < len(A) else 0
-            else:
-                i += 1
-                cur_sum -= A[i - 1]
+        P = [0]
+        for x in A: P.append(P[-1] + x)
+        count = collections.Counter()
 
-        while i < len(A):
-            if cur_sum < S:
-                break
-            if cur_sum == S:
-                ans += 1
-                i += 1
-                cur_sum -= A[i - 1]
+        ans = 0
+        for x in P:
+            ans += count[x]
+            count[x + S] += 1
+
         return ans
 
 
-print(Solution().numSubarraysWithSum([1, 0, 1, 0, 1], 2))
